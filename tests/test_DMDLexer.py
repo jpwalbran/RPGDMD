7b204e3580bf696c50ae9b9ed39a3939447afd25
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2023-03-08 22:30:32 -0600

    Add skeleton for unit testing
    Added skeleton files for spec files for the Parser and Lexer.

---
+++ tests/test_DMDLexer.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+class TestDMDLexer(TestCase):
+
+    def test_always_pass(self):
+        assert True
+    
---
+++ tests/test_DMDParser.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+class TestDMDParser(TestCase):
+
+    def test_always_pass(self):
+        assert True
