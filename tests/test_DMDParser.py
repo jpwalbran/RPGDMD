ff1b2da848598c9fc2904171673ad382d917f35b
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2023-03-09 20:25:36 -0600

    Move previous parser tests
    Restored stashed changes for simple parser unit tests.

--- tests/test_DMDParser.py
+++ tests/test_DMDParser.py
@@ -1,6 +1,18 @@
 import pytest
+from RPGDMD.DMDLexer import DMDLexer
+from RPGDMD.DMDParser import DMDParser
 
 class TestDMDParser(object):
 
-    def test_always_pass(self):
-        assert True
+    def setup_class(self):
+        self.lexer = DMDLexer()
+        self.parser = DMDParser()
+    
+    def parse(self, commandIn):
+        tokenList = self.lexer.tokenize(commandIn)
+        return self.parser.parse(tokenList)
+
+    def test_single_material_definition(self):
+        command = "matdef \"goop\" 'g'"
+        output = self.parse(command)
+        assert len(output) == 1
